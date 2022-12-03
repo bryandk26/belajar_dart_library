116e7c672bd161365fde840581cfc5b80561634d
unknown <bryandarmawan13@gmail.com>
2022-12-03 19:52:18 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_dart_library/hello.dart';
+
+void main(){
+  print(sayHello('Bryan'));
+
+  Customer customer = Customer();
+  Category category = Category();
+  Product product = Product();
+}
